c09152aad456583bda818a9ee9bf2b78dcbb59a2
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-05-15 15:12:22 +0300

    Добавьте файлы проекта.

---
+++ the_program_for_printing/menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace the_program_for_printing
+{
+    /// <summary>
+    /// Логика взаимодействия для menu.xaml
+    /// </summary>
+    public partial class menu : Page
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new one());
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ the_program_for_printing/one.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing; // Нужен для работы с PrintPageEventArgs
+using System.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents.DocumentStructures;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Diagnostics;
+
+namespace the_program_for_printing
+{
+    /// <summary>
+    /// Логика взаимодействия для one.xaml
+    /// </summary>
+    public partial class one : Page
+    {
+        public one()
+        {
+            InitializeComponent();
+        }
+
+        private string text = "";//переменная которая содержит текст для печати
+        private void prints_Click(object sender, RoutedEventArgs e)
+        {
+            text = txt_surname.Text;//текст который печатаем
+            text = txt_name.Text;//текст который печатаем
+            text = txt_lname.Text;//текст который печатаем
+
+            PrintDocument printa = new PrintDocument();
+            printa.PrintPage += PrintPageHandler;
+
+           
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(print, "Печать");
+            }
+        }
+
+   
+        void PrintPageHandler(object sender, PrintPageEventArgs e) //обработчик события печати
+        {
+            e.Graphics.DrawString(text, new System.Drawing.Font("Arial", 14), System.Drawing.Brushes.Black, 0,0);
+        }   
+    }
+}
